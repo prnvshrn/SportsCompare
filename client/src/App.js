54c61e3e1419cac99d29d467f12522549819557f
prnvshrn <prnvshrn@gmail.com>
2018-07-10 14:05:09 -0700

    Edited UI

--- client/src/App.js
+++ client/src/App.js
@@ -55,28 +55,32 @@ class PlayerPanel extends Component{
     return(
     <div>
     <Grid>
+        <Panel className="no_border">
+    <Panel.Body>
         <Row>
-          <h3><Image src="./Soccer-ball-icon.png" className="vs_icon_img" rounded /> FOOTBALL</h3>
+          <h3><Image src="./Soccer-Ball-icon.png" className="vs_icon_img" rounded /> FOOTBALL</h3>
         </Row>
         <Row>
           Please enter full name of players, eg. Steven Gerrard
         </Row>
+    </Panel.Body>
+    </Panel>    
         <Row>
         <form onSubmit={this.handleSubmit2}>
-          <Col xs={5} md={5}>
+          <Col xs={3} md={5}>
             <FormGroup>            
             <FormControl type="text" value={this.state.value} placeholder="Enter a player's name" onChange={this.handleChange} />
             </FormGroup>
           </Col>
-          <Col xs={1} md={1}>
+          <Col xs={4} md={1}>
             <Image src="./vs_icon.png" className="vs_icon_img" rounded/>
           </Col>
-          <Col xs={5} md={5}>
+          <Col xs={7} md={5}>
             <FormGroup>
             <FormControl type="text" value={this.state.value2} placeholder="Enter a player's name" onChange={this.handleChange2} />
             </FormGroup>
           </Col>
-          <Col xs={1} md={1}>
+          <Col xs={4} md={1}>
             <FormGroup>
             <Button type="submit" bsStyle="success">Go</Button>
             </FormGroup>
@@ -87,20 +91,51 @@ class PlayerPanel extends Component{
     
         {this.state.player2_stats.map(player2_stats => 
           <div key={player2_stats.name}>
-                <StatComponent 
-                name1={this.state.player1_stats[0].name} name2={this.state.player2_stats[0].name} position1={this.state.player1_stats[0].position} position2={this.state.player2_stats[0].position}
-                goals1={this.state.player1_stats[0].goals} goals2={this.state.player2_stats[0].goals} assists1={this.state.player1_stats[0].assists} assists2={this.state.player2_stats[0].assists} 
-                shooting_accuracy1={this.state.player1_stats[0].shooting_accuracy} shooting_accuracy2={this.state.player2_stats[0].shooting_accuracy}  
-                big_chance_created1={this.state.player1_stats[0].big_chance_created} big_chance_created2={this.state.player2_stats[0].big_chance_created}
-                big_chances_miss1={this.state.player1_stats[0].big_chances_miss} big_chances_miss2={this.state.player2_stats[0].big_chances_miss}
-                accurate_long_balls1={this.state.player1_stats[0].accurate_long_balls} accurate_long_balls2={this.state.player2_stats[0].accurate_long_balls}
-                tackle_success1={this.state.player1_stats[0].tackle_success} tackle_success2={this.state.player2_stats[0].tackle_success}
-                duel_won1={this.state.player1_stats[0].duel_won} duel_won2={this.state.player2_stats[0].duel_won} 
-                aerial_battle_won1={this.state.player1_stats[0].aerial_battle_won} aerial_battle_won2={this.state.player2_stats[0].aerial_battle_won}
-                interceptions1={this.state.player1_stats[0].interceptions} interceptions2={this.state.player2_stats[0].interceptions}
-                blocked_shots1={this.state.player1_stats[0].blocked_shots} blocked_shots2={this.state.player2_stats[0].blocked_shots}
-                error_to_goal1={this.state.player1_stats[0].error_to_goal} error_to_goal2={this.state.player2_stats[0].error_to_goal}
-                />
+        <Grid>
+      <Well bsSize="small" className="styled_font">
+        <Row>
+          <Col xs={3} md={2}>
+            Name
+          </Col>
+          <Col xs={4} md={3}>
+            {this.state.player1_stats[0].name}
+          </Col>
+          <Col xs={7} md={5}>
+          </Col>
+          <Col xs={4} md={2}>
+            {this.state.player2_stats[0].name}
+          </Col>  
+        </Row>
+                    
+        <div className="horizontal-line"></div>
+        <Row>
+          <Col xs={3} md={2}>
+            Position
+          </Col>
+          <Col xs={4} md={3}>
+            {this.state.player1_stats[0].position}
+          </Col>
+          <Col xs={7} md={5}>
+          </Col>
+          <Col xs={4} md={2}>
+            {this.state.player2_stats[0].position}
+          </Col>  
+        </Row>
+
+        <PlayerStateComponent stat='Goals' stat1={this.state.player1_stats[0].goals} stat2={this.state.player2_stats[0].goals} />  
+        <PlayerStateComponent stat='Shooting Accuracy' stat1={this.state.player1_stats[0].shooting_accuracy} stat2={this.state.player2_stats[0].shooting_accuracy} />
+        <PlayerStateComponent stat='Big Chances Created' stat1={this.state.player1_stats[0].big_chance_created} stat2={this.state.player2_stats[0].big_chance_created} />
+        <PlayerStateComponent stat='Big Chances Missed' stat1={this.state.player1_stats[0].big_chances_miss} stat2={this.state.player2_stats[0].big_chances_miss} />
+        <PlayerStateComponent stat='Accurate Long Balls' stat1={this.state.player1_stats[0].accurate_long_balls} stat2={this.state.player2_stats[0].accurate_long_balls} />
+        <PlayerStateComponent stat='Tackle Success' stat1={this.state.player1_stats[0].tackle_success} stat2={this.state.player2_stats[0].tackle_success} />
+        <PlayerStateComponent stat='Duels Won' stat1={this.state.player1_stats[0].duel_won} stat2={this.state.player2_stats[0].duel_won} />
+        <PlayerStateComponent stat='Aerial Battle Won' stat1={this.state.player1_stats[0].aerial_battle_won} stat2={this.state.player2_stats[0].aerial_battle_won} />
+        <PlayerStateComponent stat='Interceptions' stat1={this.state.player1_stats[0].interceptions} stat2={this.state.player2_stats[0].interceptions} />
+        <PlayerStateComponent stat='Blocked Shots' stat1={this.state.player1_stats[0].blocked_shots} stat2={this.state.player2_stats[0].blocked_shots} />
+        <PlayerStateComponent stat='Error To Goals' stat1={this.state.player1_stats[0].error_to_goal} stat2={this.state.player2_stats[0].error_to_goal} />
+
+      </Well>
+    </Grid>      
           </div>
         )}
     </div>
@@ -147,28 +182,32 @@ class BaseballPlayerPanel extends Component{
     return(
     <div>
     <Grid>
+    <Panel className="no_border">
+    <Panel.Body>
         <Row>
           <h3><Image src="./baseball_icon.png" className="vs_icon_img" rounded /> BASEBALL</h3>
         </Row>
         <Row>
           Please enter full name of players, eg. David Ortiz
         </Row>
+    </Panel.Body>
+    </Panel>    
         <Row>
         <form onSubmit={this.handleSubmit2}>
-          <Col xs={5} md={5}>
+          <Col xs={7} md={5}>
             <FormGroup>            
             <FormControl type="text" value={this.state.value} placeholder="Enter a player's name" onChange={this.handleChange} />
             </FormGroup>
           </Col>
-          <Col xs={1} md={1}>
+          <Col xs={2} md={1}>
             <Image src="./vs_icon.png" className="vs_icon_img" rounded /> 
           </Col>
-          <Col xs={5} md={5}>
+          <Col xs={7} md={5}>
             <FormGroup>
             <FormControl type="text" value={this.state.value2} placeholder="Enter a player's name" onChange={this.handleChange2} />
             </FormGroup>
           </Col>
-          <Col xs={1} md={1}>
+          <Col xs={2} md={1}>
             <FormGroup>
             <Button type="submit" bsStyle="success">Go</Button>
             </FormGroup>
@@ -177,36 +216,35 @@ class BaseballPlayerPanel extends Component{
         </Row>
     </Grid>
 
-
     {this.state.player2_stats.map(player2_stats =>
       <div key={player2_stats.name}>
     <Grid>
-      <Well bsSize="small">
+      <Well bsSize="small" className="styled_font">
         <Row>
-          <Col xs={2} md={2}>
+          <Col xs={3} md={2}>
             Name
           </Col>
-          <Col xs={3} md={3}>
+          <Col xs={4} md={3}>
             {this.state.player1_stats[0].name}
           </Col>
-          <Col xs={5} md={5}>
+          <Col xs={7} md={5}>
           </Col>
-          <Col xs={2} md={2}>
+          <Col xs={4} md={2}>
             {this.state.player2_stats[0].name}
           </Col>  
         </Row>
                     
         <div className="horizontal-line"></div>
         <Row>
-          <Col xs={2} md={2}>
+          <Col xs={3} md={2}>
             Position
           </Col>
-          <Col xs={3} md={3}>
+          <Col xs={4} md={3}>
             {this.state.player1_stats[0].position}
           </Col>
-          <Col xs={5} md={5}>
+          <Col xs={7} md={5}>
           </Col>
-          <Col xs={2} md={2}>
+          <Col xs={4} md={2}>
             {this.state.player2_stats[0].position}
           </Col>  
         </Row>
@@ -269,6 +307,26 @@ class NavbarComponent extends Component{
   }
 }
 
+class AppFooter extends Component{
+  render(){
+    return(
+      <Navbar fixedBottom collapseOnSelect>
+    <Navbar.Text>
+      Developed by Pranav Sharan
+    </Navbar.Text>
+     <Nav pullRight>
+      <NavItem eventKey={1} href="https://github.com/prnvshrn/SportsCompare">
+        GitHub
+      </NavItem>
+      <NavItem eventKey={2} href="https://www.linkedin.com/in/pranav-sharan">
+        LinkedIn
+      </NavItem>
+    </Nav>
+      </Navbar>
+      );
+  }
+}
+
 class App extends Component {
   constructor(props){
     super(props);
@@ -301,7 +359,8 @@ class App extends Component {
     return (      
       <div className="App">
       <NavbarComponent handler={this.handler}/>
-      {show_panel}   
+      {show_panel}  
+      <AppFooter/> 
       </div>
     );
   }
--- client/src/baseball_stat_component.js
+++ client/src/baseball_stat_component.js
@@ -8,19 +8,19 @@ class PlayerStatComponent extends Component{
 			<div>
                     <div className="horizontal-line"></div>
                     <Row>
-                      <Col xs={2} md={2}>
+                      <Col xs={3} md={2}>
                         {this.props.stat}
                       </Col>
-                      <Col xs={3} md={3}>
+                      <Col xs={4} md={3}>
                         {this.props.stat1}
                       </Col>
-                      <Col xs={5} md={5}>
+                      <Col xs={7} md={5}>
                         <ProgressBar>
                       <ProgressBar now={parseInt(this.props.stat1) * 100 / (parseInt(this.props.stat1) + parseInt(this.props.stat2))} key={1} />
                       <ProgressBar bsStyle="info" now={parseInt(this.props.stat2) * 100 / (parseInt(this.props.stat1) + parseInt(this.props.stat2))} key={2} />
                       </ProgressBar>
                       </Col>
-                      <Col xs={2} md={2}>
+                      <Col xs={4} md={2}>
                         {this.props.stat2}
                       </Col>  
                     </Row>
